6a6ef1f39b40c5f70322b712915f070077227980
Eva Bartolome <eva.bartolome@mrjaffesclass.com>
2023-11-06 11:19:43 -0800

    delmimiter

--- MyScanner.java
+++ MyScanner.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class MyScanner
 {
-
     public void run() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useDelimiter(",|\\n");
         while (true) {
             if (scanner.hasNext()){
-                String str = scanner.next();
-                System.out.println(str);
+                String word = scanner.next(); //int str = scanner.nextInt();
+                System.out.println(word);//System.out.println(str*3);
             }
         }
     }
